c3611ad601d360496fa598ef70c624b8acde2d8e
Nonim Udara <nonimudara123@gmail.com>
2023-03-21 12:38:10 +0530

    update home component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,27 +7,28 @@ import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg
 
 export default class Home extends Component {
 
-    render() {
-        return (
+  render() {
+    return (
 
-    <MDBFooter style={{ backgroundColor: '#3C3C3C' }} className="font-small pt-4 mt-4">
-          <MDBContainer fluid className="text-center text-md-left">
-            <MDBRow>
-              <p style={{ fontWeight: 'regular', color: '#CFCFCF', fontSize: '19px' }}>Vehicle Automotive spare parts sales platform</p>
-              <hr style={{ color: '#CFCFCF', alignItems: 'center', textAlign: 'center' }}></hr>
-            </MDBRow>
+      <MDBFooter style={{ backgroundColor: '#3C3C3C' }} className="font-small pt-4 mt-4">
+        <MDBContainer fluid className="text-center text-md-left">
+          <MDBRow>
+            <p style={{ fontWeight: 'regular', color: '#CFCFCF', fontSize: '19px' }}>Vehicle Automotive spare parts sales platform</p>
+            <hr style={{ color: '#CFCFCF', alignItems: 'center', textAlign: 'center' }}></hr>
+          </MDBRow>
+        </MDBContainer>
+        <div className="footer-copyright text-center py-3">
+          <h1 style={{ marginBottom: "20px", textAlign: "center", alignItems: "center" }}>
+            <a href=" " role="button" style={{ padding: "10px", color: "#4267B2" }}><FontAwesomeIcon icon={faFacebook} /></a>
+            <a href=" " role="button" style={{ padding: "10px", color: "#1DA1F2" }}><FontAwesomeIcon icon={faTwitter} /></a>
+            <a href=" " role="button" style={{ padding: "10px", color: "#833AB4" }}><FontAwesomeIcon icon={faInstagram} /></a>
+          </h1>
+          <MDBContainer fluid>
+            <p style={{ fontWeight: 'regular', color: '#CFCFCF', fontSize: '15px' }}>Nonim Creations © 2023</p>
+            <p style={{ fontWeight: 'regular', color: '#CFCFCF', fontSize: '15px' }}>All Right Reserved</p>
           </MDBContainer>
-          <div className="footer-copyright text-center py-3">
-            <h1 style={{ marginBottom: "20px", textAlign: "center", alignItems: "center" }}>
-              <a href=" " role="button" style={{ padding: "10px", color: "#4267B2" }}><FontAwesomeIcon icon={faFacebook} /></a>
-              <a href=" " role="button" style={{ padding: "10px", color: "#1DA1F2" }}><FontAwesomeIcon icon={faTwitter} /></a>
-              <a href=" " role="button" style={{ padding: "10px", color: "#833AB4" }}><FontAwesomeIcon icon={faInstagram} /></a>
-            </h1>
-            <MDBContainer fluid>
-              <p style={{ fontWeight: 'regular', color: '#CFCFCF', fontSize: '15px' }}>Nonim Creations © 2023</p>
-              <p style={{ fontWeight: 'regular', color: '#CFCFCF', fontSize: '15px' }}>All Right Reserved</p>
-            </MDBContainer>
-          </div>
-        </MDBFooter>
-        )};
+        </div>
+      </MDBFooter>
+    )
+  };
 }
