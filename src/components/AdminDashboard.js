5831483ca1a480f98577066fd53977a5220ba4a0
Nonim Udara <nonimudara123@gmail.com>
2023-05-18 16:44:02 +0530

    update admin dashboard

--- src/components/AdminDashboard.js
+++ src/components/AdminDashboard.js
@@ -18,26 +18,22 @@ export default class AdminDashboard extends Component {
                 <p>Parts Management</p>
                 <a className="btn btn-outline-primary w-25" href="/partsdetails" role="button">Parts</a>
               </div>
-              <hr style={{ width: '70%', margin: 'auto' }}></hr>
-
-              <h3 style={{ textAlign: 'center', marginBottom: '20px' }}>Garage Locations</h3>
+              <hr style={{ width: '70%', margin: 'auto', marginTop: 30, marginBottom: 30 }}></hr>
               <div className="col-12" style={{ alignItems: 'center', textAlign: 'center' }}>
                 <p>Manage Location Send By Users</p>
-                <a className="btn btn-outline-success w-25" href="garagesdetails" role="button">Manage</a>
+                <a className="btn btn-outline-success w-25" href="garagesdetails" role="button" style={{ marginTop: 30, marginBottom: 30 }}>Manage</a>
               </div>
-              <hr style={{ marginTop: '30px', marginBottom: '30px' }}></hr>
-
-              <div className="col-6" style={{ alignItems: 'center', textAlign: 'center' }}>
+              <hr style={{ width: '70%', margin: 'auto' }}></hr>
+              <div className="col-6" style={{ alignItems: 'center', textAlign: 'center', marginTop: 30, marginBottom: 30 }}>
                 <p>View Checkouts</p>
                 <a className="btn btn-outline-primary w-25" href="/memberdetails" role="button">Checkouts</a>
               </div>
-              <div className="col-6" style={{ alignItems: 'center', textAlign: 'center' }}>
+              <div className="col-6" style={{ alignItems: 'center', textAlign: 'center', marginTop: 30, marginBottom: 30 }}>
                 <p>View Contact Us Messages</p>
-                <a className="btn btn-outline-primary w-25" href="contactusdetails" role="button">Messages</a>
+                <a className="btn btn-outline-primary w-25" href="contactusdetails" role="button" style={{ marginBottom: 30 }}>Messages</a>
               </div>
-              <hr style={{ marginTop: '30px', marginBottom: '30px', }}></hr>
-
-              <div className="col-12" style={{ alignItems: 'center', textAlign: 'center' }}>
+              <hr style={{ width: '70%', margin: 'auto' }}></hr>
+              <div className="col-12" style={{ alignItems: 'center', textAlign: 'center', marginTop: 30, marginBottom: 30 }}>
                 <a className="btn btn-outline-danger w-25" href="/" role="button">Logout</a>
               </div>
 
